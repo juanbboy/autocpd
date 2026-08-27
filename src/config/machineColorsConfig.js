1f3fd4cb9522e3e3aa7d4fea0ddf609809883e9c
Desarrollo Nylon <juanca.rr@hotmail.com>
2026-08-27 16:05:19 -0500

    first commit

--- src/config/machineColorsConfig.js
+++ src/config/machineColorsConfig.js
@@ -8,15 +8,20 @@ const cpdazul = require('../assets/cpdazul.png');
 const cpdcian = require('../assets/cpdcian.png');
 
 export const machineColorsMap = {
-    1: cpdrojo,      // Mecánico - Rojo
-    2: cpdnegro,     // Barrado - Negro
-    3: cpdamarillo,  // Electrónico - Amarillo
-    7: cpd,          // Fin Producción - Blanco
-    5: cpdcian,     // Seguimiento - Cian
-    6: cpdazul,       // Tallaje - Azul
-    4: cpdverde      // Inicio Producción - Verde (puedes cambiarlo si quieres otro color)
-};
-
+    1: cpdamarillo,
+    3: cpdamarillo,
+    8: cpdamarillo,
+    13: cpdamarillo,
+    11: cpdamarillo,
+    10: cpdamarillo,
+    2: cpdcian,
+    6: cpdnegro,
+    9: cpdrojo,
+    16: cpdazul,
+    15: cpdverde,
+    4: cpdverde,
+    7: cpd
+}
 // /**
 //  * Función auxiliar para obtener la imagen de una máquina
 //  * @param {number} main - ID del estado principal
