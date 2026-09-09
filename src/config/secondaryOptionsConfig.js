6e6818a73980d081218a8240dd5c1263d010778f
Desarrollo Nylon <juanca.rr@hotmail.com>
2026-09-09 15:25:59 -0500

    first commit

--- src/config/secondaryOptionsConfig.js
+++ src/config/secondaryOptionsConfig.js
@@ -14,7 +14,7 @@ export const secondaryOptionsMap = {
         { label: "Daño de cuchillas", code: "DI03" },
 
         "Transferencia", "Reviente LC", "Succion", "Reviente L180", "Piques",
-        "Huecos y rotos", "Motores MPP", "Cuchillas", "correa",
+        "Huecos y rotos", "Barrado mot. MPP", "Cuchillas", "correa",
         "Manguera rota", "Lubricacion", "Guia hilos", "Otros", "Limpieza", "Trasdenuto", "Escaricato"
     ],
     2: [
@@ -44,7 +44,7 @@ export const secondaryOptionsMap = {
     ],
     7: [],
     9: [
-        "Aguja -10", "Aguja +10"
+        "Aguja -10", "Aguja +10", "Piques", "Platinas"
     ],
     16: [
         { label: "Inasistencia ", code: "DI19" },
